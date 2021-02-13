a1191f83b046c253f72e3349d58ece116d0e94f4
sarah-knowles <sjk339@gmail.com>
2021-02-14 12:03:23 +1300

    addStringsOrNumbers-tbc

--- utilities.js
+++ utilities.js
@@ -1,22 +1,34 @@
 function getType (thing) {
+  return typeof thing
 }
 
 function isNumber (thing) {
+  return typeof thing === 'number'
 }
 
 function toNumber (str) {
+  return parseInt(str)
 }
 
 function isStringNumber (str) {
+  var num = toNumber(str)
+  var isNum = typeof num === 'number'
+  var isNotNan = !isNaN(num)
+  return isNum && isNotNan 
 }
 
 function add (a, b) {
+  return a + b
 }
 
 function addStrings (a, b) {
+  const aNum = toNumber(a)
+  const bNum = toNumber(b)
+  return String(aNum + bNum)
 }
 
 function addStringsOrNumbers (a, b) {
+  
 }
 
 function isEmail (str) {
@@ -37,3 +49,12 @@ function filterStringsWithCommas (str) {
 function splitStringByCommas (str) {
 }
 
+module.exports = {
+  getType: getType,
+  isNumber,
+  toNumber,
+  isStringNumber,
+  add,
+  addStrings,
+  addStringsOrNumbers
+} 
